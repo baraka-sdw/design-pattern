8938a6383a4fd28253d51aaa9fc889b2109183d5
Baraka <1348286107@qq.com>
2019-11-21 14:07:35 +0800

    原型模式与适配器模式

---
+++ PrototypePattern/Rectangle.java
@@ -0,0 +1,14 @@
+/**
+ * 创建扩展了上面抽象类的实体类
+ */
+public class Rectangle extends Shape {
+ 
+    public Rectangle(){
+      type = "Rectangle";
+    }
+  
+    @Override
+    public void draw() {
+       System.out.println("Inside Rectangle::draw() method.");
+    }
+ }
